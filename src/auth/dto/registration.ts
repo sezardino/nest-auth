953c8f76e61f6eaa9a3dbc0b5b4f67453c817fdd
Edward Arechwa <e.arechwa@gmail.com>
2022-03-14 19:21:45 +0100

    add validation for body

--- src/auth/dto/registration.ts
+++ src/auth/dto/registration.ts
@@ -1,5 +1,17 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class RegistrationDto {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
+  @MinLength(8)
   readonly email: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly password: string;
 }
